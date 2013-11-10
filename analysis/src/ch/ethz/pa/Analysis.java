cba8cba1ed17a222bbd344be9a42ff822a468614
cfuchs <cfuchs77@gmail.com>
2013-11-10 21:30:42 +0100

    moved DefinitionStmt code to separate class

--- analysis/src/ch/ethz/pa/Analysis.java
+++ analysis/src/ch/ethz/pa/Analysis.java
@@ -34,8 +34,10 @@ public class Analysis extends ForwardBranchedFlowAnalysis<IntervalPerVar> {
 	
 	private final ProblemReport problemReport = new ProblemReport();
 	
-	private final Interval legalSensorInterval = new Interval(0, 15);
-	private final Interval legalValueInterval = new Interval(-999,999);
+	public static final Interval legalSensorInterval = new Interval(0, 15);
+	public static final Interval legalValueInterval = new Interval(-999,999);
+	
+	private final DefinitionStmtAnalyzer definitionStmtAnalyzer = new DefinitionStmtAnalyzer(problemReport);
 	
 	public Analysis(UnitGraph g) {
 		super(g);
@@ -63,7 +65,7 @@ public class Analysis extends ForwardBranchedFlowAnalysis<IntervalPerVar> {
 		branchState.copyFrom(current);
 
 		if (s instanceof DefinitionStmt) {
-			flowThroughDefinitionStmt(current, (DefinitionStmt) s, fallState);
+			definitionStmtAnalyzer.analyze(current, (DefinitionStmt) s, fallState);
 		} 
 		
 		else if (s instanceof JInvokeStmt) {
@@ -116,81 +118,6 @@ public class Analysis extends ForwardBranchedFlowAnalysis<IntervalPerVar> {
 		}
 	}
 
-	private void flowThroughDefinitionStmt(IntervalPerVar current,
-			DefinitionStmt sd, IntervalPerVar fallState) {
-
-		Value left = sd.getLeftOp();
-		Value right = sd.getRightOp();
-		logger.info(left.getClass().getName() + " " + right.getClass().getName());
-
-		// You do not need to handle these cases:
-		if ((!(left instanceof StaticFieldRef)) && (!(left instanceof JimpleLocal)) && (!(left instanceof JArrayRef))
-				&& (!(left instanceof JInstanceFieldRef)))
-			unhandled("1: Assignment to non-variables is not handled.");
-		
-		else if ((left instanceof JArrayRef) && (!((((JArrayRef) left).getBase()) instanceof JimpleLocal)))
-			unhandled("2: Assignment to a non-local array variable is not handled.");
-		
-		// TODO: Handle other cases. For example:
-
-		else if (left instanceof JimpleLocal) {
-			
-			JimpleLocal jimpleLocalLeft = (JimpleLocal) left;
-			String varName = jimpleLocalLeft.getName();
-			
-
-			if (jimpleLocalLeft.getType() instanceof RefType) {
-				logger.warning("ignoring assignments to complex type: "+sd);
-			}
-			
-			else if (right instanceof IntConstant) {
-				IntConstant c = ((IntConstant) right);
-				fallState.putIntervalForVar(varName, new Interval(c.value, c.value));
-			} 
-			
-			else if (right instanceof JimpleLocal) {
-				JimpleLocal l = ((JimpleLocal) right);
-				if (l.getType() instanceof RefLikeType) {
-					logger.warning("ignore right side "+l.getType());
-				}
-				else {
-					fallState.putIntervalForVar(varName, current.getIntervalForVar(l.getName()));
-					throw new RuntimeException("hit unexpected type "+l.getType());
-				}
-			} 
-			
-			else if (right instanceof BinopExpr) {
-				Interval result = IntegerExpression.evalBinop((BinopExpr) right, current);
-				fallState.putIntervalForVar(varName, result);
-			}
-			
-			else if (right instanceof JVirtualInvokeExpr) {
-				JVirtualInvokeExpr expr = (JVirtualInvokeExpr)right;
-				SootMethod method = expr.getMethodRef().resolve();
-				if (method.getName().equals("readSensor")) {
-					if (method.getDeclaringClass().getName().equals("AircraftControl")) {
-						problemReport.checkInterval(expr.getArg(0), legalSensorInterval, current);
-						fallState.putIntervalForVar(varName, new Interval(-999, 999));
-					}
-				}
-			}
-
-			else if (right instanceof UnopExpr) {
-				Interval result = IntegerExpression.evalUnop((UnopExpr) right, current);
-				fallState.putIntervalForVar(varName, result);
-			}
-
-			else {
-				throw new RuntimeException("unhandled JimpleLocal "+left+ " at "+sd);
-			}
-			// ...
-		}
-		
-		else {
-			throw new RuntimeException("unhandled lhs "+left);
-		}
-		// ...
-	}
 
 	@Override
 	protected void copy(IntervalPerVar source, IntervalPerVar dest) {
