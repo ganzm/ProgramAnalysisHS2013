0c82c9dbd5f147625de1fdd7f2959574499f1ea1
Matthias Ganz <gaenzele@gmail.com>
2013-11-12 09:41:47 +0100

    removed some warnings

--- analysisTest/frameworkTests/ch/ethz/pa/test/frameworkTests/AnalysisTest.java
+++ analysisTest/frameworkTests/ch/ethz/pa/test/frameworkTests/AnalysisTest.java
@@ -3,8 +3,7 @@ package ch.ethz.pa.test.frameworkTests;
 import java.util.Arrays;
 import java.util.List;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,41 +21,41 @@ import ch.ethz.pa.Interval;
 import ch.ethz.pa.IntervalPerVar;
 
 public class AnalysisTest {
-	
+
 	protected final JimpleBody simpleBody = createSimpleBody();
 
 	/**
 	 * Provides a body that consists of only a return statement.
+	 * 
 	 * @return
 	 */
 	private static JimpleBody createSimpleBody() {
 		// setup global object
 		Scene.v().loadClassAndSupport("java.lang.Object");
-		
+
 		// setup class
 		SootClass sClass = new SootClass("MyClass", Modifier.PUBLIC);
 		sClass.setSuperclass(Scene.v().getSootClass("java.lang.Object"));
 		Scene.v().addClass(sClass);
-		
+
 		// setup method
-		SootMethod method = new SootMethod("myMethod",                 
-		        Arrays.asList(new Type[] {}), VoidType.v(), Modifier.PUBLIC | Modifier.STATIC);
+		SootMethod method = new SootMethod("myMethod", Arrays.asList(new Type[] {}), VoidType.v(), Modifier.PUBLIC | Modifier.STATIC);
 		sClass.addMethod(method);
-		
+
 		// setup body
 		JimpleBody body = Jimple.v().newBody(method);
-	    method.setActiveBody(body);
-	    
-	    // add return stmt
-	    body.getUnits().add(Jimple.v().newReturnVoidStmt());
+		method.setActiveBody(body);
+
+		// add return stmt
+		body.getUnits().add(Jimple.v().newReturnVoidStmt());
 		return body;
 	}
-	
+
 	Analysis analysis;
 	IntervalPerVar src1;
 	IntervalPerVar src2;
 	IntervalPerVar trg;
-	
+
 	@Before
 	public void setup() {
 		analysis = new Analysis(new BriefUnitGraph(simpleBody));
@@ -64,19 +63,19 @@ public class AnalysisTest {
 		src2 = new IntervalPerVar();
 		trg = new IntervalPerVar();
 	}
-	
+
 	@Test
 	public void testInitialErrorFeedback() {
 		List<String> problems = analysis.getProblems();
 		Assert.assertNotNull("expected a list", problems);
 		Assert.assertEquals("expected an empty list", 0, problems.size());
-	} 
-	
-	
+	}
+
 	/**
 	 * Create an {@link Analysis} with one problem.
+	 * 
 	 * @author cfuchs
-	 *
+	 * 
 	 */
 	private class AnalysisWithOneProblemMock extends Analysis {
 		public AnalysisWithOneProblemMock() {
@@ -84,34 +83,35 @@ public class AnalysisTest {
 			super.addProblem("Houston we have a problem");
 		}
 	}
-	
+
 	@Test
 	public void testHavingErrorFeedback() {
 		List<String> problems = new AnalysisWithOneProblemMock().getProblems();
 		Assert.assertEquals("expected a problem", 1, problems.size());
 	}
-	
+
 	private class AnalysisWithPublicMerge extends Analysis {
 		public AnalysisWithPublicMerge() {
 			super(new BriefUnitGraph(simpleBody));
 		}
+
 		@Override
 		public void merge(IntervalPerVar src1, IntervalPerVar src2, IntervalPerVar trg) {
 			super.merge(src1, src2, trg);
 		}
-		
+
 	}
-	
+
 	@Test
 	public void testMergeDisjunct() {
-		
+
 		AnalysisWithPublicMerge analysis = new AnalysisWithPublicMerge();
-		
+
 		src1.putIntervalForVar("A", new Interval(1));
 		src2.putIntervalForVar("A", new Interval(3));
 
 		analysis.merge(src1, src2, trg);
-		
+
 		Assert.assertTrue(trg.getIntervalForVar("A").covers(new Interval(1)));
 		Assert.assertTrue(trg.getIntervalForVar("A").covers(new Interval(3)));
 	}
--- analysisTest/frameworkTests/ch/ethz/pa/test/frameworkTests/IntervalTests.java
+++ analysisTest/frameworkTests/ch/ethz/pa/test/frameworkTests/IntervalTests.java
@@ -1,7 +1,6 @@
 package ch.ethz.pa.test.frameworkTests;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 import ch.ethz.pa.Interval;
@@ -23,89 +22,87 @@ public class IntervalTests {
 
 	@Test
 	public void testRangeInRange() {
-		Assert.assertTrue("subset should be covered", new Interval(10, 15).covers(new Interval(11,14)));
-		Assert.assertTrue("equal set should be covered", new Interval(10, 15).covers(new Interval(10,15)));
+		Assert.assertTrue("subset should be covered", new Interval(10, 15).covers(new Interval(11, 14)));
+		Assert.assertTrue("equal set should be covered", new Interval(10, 15).covers(new Interval(10, 15)));
 	}
 
 	@Test
 	public void testRangeOutOfRange() {
-		Assert.assertFalse("underrun not considered", new Interval(10, 15).covers(new Interval(9,15)));
-		Assert.assertFalse("overrun not considered", new Interval(10, 15).covers(new Interval(10,16)));
+		Assert.assertFalse("underrun not considered", new Interval(10, 15).covers(new Interval(9, 15)));
+		Assert.assertFalse("overrun not considered", new Interval(10, 15).covers(new Interval(10, 16)));
 	}
-	
+
 	@Test
 	public void testMultiply() {
-		Assert.assertEquals(new Interval(-9,3), Interval.multiply(new Interval(-3,1), new Interval(-1,3)));
-		Assert.assertEquals(new Interval(-4,0), Interval.multiply(new Interval(-2,0), new Interval(0,2)));
-		Assert.assertEquals(new Interval(0,0), Interval.multiply(new Interval(-5,5), new Interval(0,0)));
+		Assert.assertEquals(new Interval(-9, 3), Interval.multiply(new Interval(-3, 1), new Interval(-1, 3)));
+		Assert.assertEquals(new Interval(-4, 0), Interval.multiply(new Interval(-2, 0), new Interval(0, 2)));
+		Assert.assertEquals(new Interval(0, 0), Interval.multiply(new Interval(-5, 5), new Interval(0, 0)));
 	}
 
 	@Test
 	public void testDivide() {
-		Assert.assertEquals(new Interval(-8,4), Interval.divide(new Interval(-80,40), new Interval(2,10)));
-		Assert.assertEquals(new Interval(-12,4), Interval.divide(new Interval(-20,60), new Interval(-5,-2)));
+		Assert.assertEquals(new Interval(-8, 4), Interval.divide(new Interval(-80, 40), new Interval(2, 10)));
+		Assert.assertEquals(new Interval(-12, 4), Interval.divide(new Interval(-20, 60), new Interval(-5, -2)));
 	}
-	
 
 	@Test
 	public void testSubtract() {
-		Assert.assertEquals(new Interval(-3,4), Interval.subtract(new Interval(-1,5), new Interval(1,2)));
+		Assert.assertEquals(new Interval(-3, 4), Interval.subtract(new Interval(-1, 5), new Interval(1, 2)));
 	}
-	
+
 	@Test
 	public void testNegate() {
-		Assert.assertEquals(new Interval(-3,7), new Interval(-7, 3).negate());
+		Assert.assertEquals(new Interval(-3, 7), new Interval(-7, 3).negate());
 	}
-	
-	@Test(expected=Exception.class)
-	public void testInvalidIntervalThrowsException()
-	{
-		new Interval(5,4);
+
+	@Test(expected = Exception.class)
+	public void testInvalidIntervalThrowsException() {
+		new Interval(5, 4);
 	}
-	
+
 	@Test
 	public void testGreaterEqual() {
 		Assert.assertEquals(TriState.True, Interval.greaterEqual(new Interval(5), new Interval(5)));
 		Assert.assertEquals(TriState.False, Interval.greaterEqual(new Interval(4), new Interval(5)));
-		Assert.assertEquals(TriState.True, Interval.greaterEqual(new Interval(5,7), new Interval(3,5)));
-		Assert.assertEquals(TriState.False, Interval.greaterEqual(new Interval(1,2), new Interval(3,5)));
-		Assert.assertEquals(TriState.Unknown, Interval.greaterEqual(new Interval(1,3), new Interval(3,5)));		
+		Assert.assertEquals(TriState.True, Interval.greaterEqual(new Interval(5, 7), new Interval(3, 5)));
+		Assert.assertEquals(TriState.False, Interval.greaterEqual(new Interval(1, 2), new Interval(3, 5)));
+		Assert.assertEquals(TriState.Unknown, Interval.greaterEqual(new Interval(1, 3), new Interval(3, 5)));
 	}
-	
+
 	@Test
 	public void testLimitToGreaterEqual() {
 		Assert.assertEquals(new Interval(1), new Interval(1).limitToGreaterEqual(new Interval(1)));
-		Assert.assertEquals(new Interval(3,4), new Interval(3,4).limitToGreaterEqual(new Interval(1,2)));
-		Assert.assertEquals(new Interval(2,4), new Interval(2,4).limitToGreaterEqual(new Interval(1,3)));
-		Assert.assertEquals(new Interval(2,4), new Interval(1,4).limitToGreaterEqual(new Interval(2,3)));
-		Assert.assertEquals(Interval.EMPTY_INTERVAL, new Interval(1,2).limitToGreaterEqual(new Interval(3,4)));
+		Assert.assertEquals(new Interval(3, 4), new Interval(3, 4).limitToGreaterEqual(new Interval(1, 2)));
+		Assert.assertEquals(new Interval(2, 4), new Interval(2, 4).limitToGreaterEqual(new Interval(1, 3)));
+		Assert.assertEquals(new Interval(2, 4), new Interval(1, 4).limitToGreaterEqual(new Interval(2, 3)));
+		Assert.assertEquals(Interval.EMPTY_INTERVAL, new Interval(1, 2).limitToGreaterEqual(new Interval(3, 4)));
 	}
 
 	@Test
 	public void testLimitToGreater() {
-		Assert.assertEquals(new Interval(3,4), new Interval(3,4).limitToGreater(new Interval(0,1)));
-		Assert.assertEquals(new Interval(2,4), new Interval(2,4).limitToGreater(new Interval(0,2)));
-		Assert.assertEquals(new Interval(2,4), new Interval(1,4).limitToGreater(new Interval(1,2)));
-		Assert.assertEquals(Interval.EMPTY_INTERVAL, new Interval(1,2).limitToGreater(new Interval(2,3)));
+		Assert.assertEquals(new Interval(3, 4), new Interval(3, 4).limitToGreater(new Interval(0, 1)));
+		Assert.assertEquals(new Interval(2, 4), new Interval(2, 4).limitToGreater(new Interval(0, 2)));
+		Assert.assertEquals(new Interval(2, 4), new Interval(1, 4).limitToGreater(new Interval(1, 2)));
+		Assert.assertEquals(Interval.EMPTY_INTERVAL, new Interval(1, 2).limitToGreater(new Interval(2, 3)));
 		Assert.assertEquals(Interval.EMPTY_INTERVAL, new Interval(1).limitToGreater(new Interval(1)));
 	}
 
 	@Test
 	public void testLimitToLowerEqual() {
-		Assert.assertEquals(Interval.EMPTY_INTERVAL, new Interval(3,4).limitToLowerEqual(new Interval(1,2)));
-		Assert.assertEquals(new Interval(2,3), new Interval(2,4).limitToLowerEqual(new Interval(1,3)));
-		Assert.assertEquals(new Interval(1,3), new Interval(1,4).limitToLowerEqual(new Interval(2,3)));
-		Assert.assertEquals(new Interval(1,2), new Interval(1,2).limitToLowerEqual(new Interval(3,4)));
+		Assert.assertEquals(Interval.EMPTY_INTERVAL, new Interval(3, 4).limitToLowerEqual(new Interval(1, 2)));
+		Assert.assertEquals(new Interval(2, 3), new Interval(2, 4).limitToLowerEqual(new Interval(1, 3)));
+		Assert.assertEquals(new Interval(1, 3), new Interval(1, 4).limitToLowerEqual(new Interval(2, 3)));
+		Assert.assertEquals(new Interval(1, 2), new Interval(1, 2).limitToLowerEqual(new Interval(3, 4)));
 		Assert.assertEquals(new Interval(1), new Interval(1).limitToLowerEqual(new Interval(1)));
 	}
 
 	@Test
 	public void testLimitToLower() {
 		Assert.assertEquals(Interval.EMPTY_INTERVAL, new Interval(1).limitToLower(new Interval(1)));
-		Assert.assertEquals(Interval.EMPTY_INTERVAL, new Interval(3,4).limitToLower(new Interval(2,3)));
-		Assert.assertEquals(new Interval(2,3), new Interval(2,4).limitToLower(new Interval(2,4)));
-		Assert.assertEquals(new Interval(1,3), new Interval(1,4).limitToLower(new Interval(3,4)));
-		Assert.assertEquals(new Interval(1,2), new Interval(1,2).limitToLower(new Interval(4,5)));
+		Assert.assertEquals(Interval.EMPTY_INTERVAL, new Interval(3, 4).limitToLower(new Interval(2, 3)));
+		Assert.assertEquals(new Interval(2, 3), new Interval(2, 4).limitToLower(new Interval(2, 4)));
+		Assert.assertEquals(new Interval(1, 3), new Interval(1, 4).limitToLower(new Interval(3, 4)));
+		Assert.assertEquals(new Interval(1, 2), new Interval(1, 2).limitToLower(new Interval(4, 5)));
 	}
 
 	@Test
@@ -119,6 +116,5 @@ public class IntervalTests {
 		Assert.assertEquals(Interval.EMPTY_INTERVAL, Interval.EMPTY_INTERVAL.limitToGreaterEqual(new Interval(5)));
 		Assert.assertEquals(Interval.EMPTY_INTERVAL, new Interval(5).limitToGreaterEqual(Interval.EMPTY_INTERVAL));
 	}
-
 
 }
