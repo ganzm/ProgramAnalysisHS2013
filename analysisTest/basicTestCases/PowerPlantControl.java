c9f3c2accf1c301605c3b6b6bf02b037b4e2176a
cfuchs <cfuchs77@gmail.com>
2013-11-14 22:26:39 +0100

    refactor: added comment

--- analysisTest/basicTestCases/PowerPlantControl.java
+++ analysisTest/basicTestCases/PowerPlantControl.java
@@ -1,12 +1,15 @@
-
 /**
- * This class has the same methods as the {@link AircraftControl},
- * but is not related to it. Therefore, the specific checks of
- * {@link AircraftControl} do not apply to it.
+ * This class has the same methods as the {@link AircraftControl}, but is not related to it.
+ * Therefore, the specific checks of {@link AircraftControl} do not apply to it.
+ * <p>
+ * 
+ * This is <b>NOT A TEST CASE</b>, but servers as a type in a test case.
  */
 public class PowerPlantControl {
 	public native int readSensor(int sensorId);
+
 	public native void adjustValue(int sensorId, int newValue);
 
-	public PowerPlantControl() {}
+	public PowerPlantControl() {
+	}
 }
