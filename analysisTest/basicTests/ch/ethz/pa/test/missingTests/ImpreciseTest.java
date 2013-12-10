6f989e3fc07c3d7b2ec6493034d006987d74ad92
Matthias Ganz <gaenzele@gmail.com>
2013-12-10 23:16:00 +0100

    added TestCases

---
+++ analysisTest/basicTests/ch/ethz/pa/test/missingTests/ImpreciseTest.java
@@ -0,0 +1,34 @@
+package ch.ethz.pa.test.missingTests;
+
+import org.junit.Test;
+
+import ch.ethz.pa.test.ValidationTestBase;
+
+public class ImpreciseTest extends ValidationTestBase {
+
+	@Test
+	public void testTestSimpleGen10_187_at_testRRR_OK_12() {
+		testAnyProgram("TestSimpleGen10_187_at_testRRR_OK_12", SAFE);
+	}
+
+	@Test
+	public void testTestSimpleGen10_199_at_testRRR_OK_24() {
+		testAnyProgram("TestSimpleGen10_199_at_testRRR_OK_24", SAFE);
+	}
+
+	@Test
+	public void testTestSimpleGen11_380_at_testOAMu_OK_8() {
+		testAnyProgram("TestSimpleGen11_380_at_testOAMu_OK_8", SAFE);
+	}
+
+	@Test
+	public void testTestSimpleGen11_384_at_testOMuM_OK_12() {
+		testAnyProgram("TestSimpleGen11_384_at_testOMuM_OK_12", SAFE);
+	}
+
+	@Test
+	public void testTestSimpleGen11_424_at_testOSMSSuuS_OK_52() {
+		testAnyProgram("TestSimpleGen11_424_at_testOSMSSuuS_OK_52", SAFE);
+	}
+
+}
---
+++ analysisTest/basicTests/ch/ethz/pa/test/missingTests/UnsoundTest.java
@@ -0,0 +1,34 @@
+package ch.ethz.pa.test.missingTests;
+
+import org.junit.Test;
+
+import ch.ethz.pa.test.ValidationTestBase;
+
+public class UnsoundTest extends ValidationTestBase {
+
+	@Test
+	public void testTestSimpleGen1_225_at_testL_AboveEnd_1() {
+		testAnyProgram("TestSimpleGen1_225_at_testL_AboveEnd_1", UNSAFE);
+	}
+
+	@Test
+	public void testTestSimpleGen1_226_at_testL_Negative_2() {
+		testAnyProgram("TestSimpleGen1_226_at_testL_Negative_2", UNSAFE);
+	}
+
+	@Test
+	public void testTestSimpleGen13_891_at_testDMD_AboveEnd_21() {
+		testAnyProgram("TestSimpleGen13_891_at_testDMD_AboveEnd_21", UNSAFE);
+	}
+
+	@Test
+	public void testTestSimpleGen13_892_at_testDMD_Negative_22() {
+		testAnyProgram("TestSimpleGen13_892_at_testDMD_Negative_22", UNSAFE);
+	}
+
+	@Test
+	public void testTestSimpleGen13_893_at_testDMD_NegativeAboveEnd_23() {
+		testAnyProgram("TestSimpleGen13_893_at_testDMD_NegativeAboveEnd_23", UNSAFE);
+	}
+
+}
