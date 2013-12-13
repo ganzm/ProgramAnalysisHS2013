76b6876c1a7b760d398d9b39acd8cc3af8810861
cfuchs <cfuchs77@gmail.com>
2013-12-13 19:59:16 +0100

    (just put timeouts on tests)

--- analysisTest/basicTests/ch/ethz/pa/test/missingTests/ImpreciseTest.java
+++ analysisTest/basicTests/ch/ethz/pa/test/missingTests/ImpreciseTest.java
@@ -6,37 +6,37 @@ import ch.ethz.pa.test.ValidationTestBase;
 
 public class ImpreciseTest extends ValidationTestBase {
 
-	@Test
+	@Test(timeout = 10000)
 	public void testTestSimpleGen10_187_at_testRRR_OK_12() {
 		testAnyProgram("TestSimpleGen10_187_at_testRRR_OK_12", SAFE);
 	}
 
-	@Test
+	@Test(timeout = 10000)
 	public void testTestSimpleGen10_199_at_testRRR_OK_24() {
 		testAnyProgram("TestSimpleGen10_199_at_testRRR_OK_24", SAFE);
 	}
 
-	@Test
+	@Test(timeout = 10000)
 	public void testTestSimpleGen11_380_at_testOAMu_OK_8() {
 		testAnyProgram("TestSimpleGen11_380_at_testOAMu_OK_8", SAFE);
 	}
 
-	@Test
+	@Test(timeout = 10000)
 	public void testTestSimpleGen11_384_at_testOMuM_OK_12() {
 		testAnyProgram("TestSimpleGen11_384_at_testOMuM_OK_12", SAFE);
 	}
 
-	@Test
+	@Test(timeout = 10000)
 	public void testTestSimpleGen11_424_at_testOSMSSuuS_OK_52() {
 		testAnyProgram("TestSimpleGen11_424_at_testOSMSSuuS_OK_52", SAFE);
 	}
 
-	@Test(timeout = 5000)
+	@Test(timeout = 10000)
 	public void testTestSimpleGen11_404_at_testOASSuAOA_OK_32() {
 		testAnyProgram("TestSimpleGen11_404_at_testOASSuAOA_OK_32", SAFE);
 	}
 
-	@Test
+	@Test(timeout = 10000)
 	public void testTestSimpleGen11_416_at_testOSSuSS_OK_44() {
 		testAnyProgram("TestSimpleGen11_416_at_testOSSuSS_OK_44", SAFE);
 	}
