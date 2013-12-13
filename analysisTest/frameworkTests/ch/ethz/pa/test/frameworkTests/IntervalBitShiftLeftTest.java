8ebfa8303ce65158dd22c77d4f2a675d6ebb3d16
cfuchs <cfuchs77@gmail.com>
2013-12-13 20:15:18 +0100

    added test checking shl with random tests (fails in some cases)

--- analysisTest/frameworkTests/ch/ethz/pa/test/frameworkTests/IntervalBitShiftLeftTest.java
+++ analysisTest/frameworkTests/ch/ethz/pa/test/frameworkTests/IntervalBitShiftLeftTest.java
@@ -1,5 +1,7 @@
 package ch.ethz.pa.test.frameworkTests;
 
+import java.util.Random;
+
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -161,6 +163,52 @@ public class IntervalBitShiftLeftTest {
 		System.out.println(BinaryUtil.toBinString(-1 >>> 32) + "= -1 >>> 32");
 		System.out.println(BinaryUtil.toBinString(-1 >>> -32) + "= -1 >>> -32");
 		System.out.println(BinaryUtil.toBinString(-1 >>> 0) + "= -1 >>> 32");
+	}
+
+	/**
+	 * Test some bit-or with random intervals, and print out precision information.
+	 */
+	@Test
+	public void testOrExpensive() {
+		for (int i = 0; i < 100; i++) {
+			Random rnd = new Random();
+			final int i1lo = rnd.nextInt(1000) - 500;
+			final int i1hi = i1lo + rnd.nextInt(30);
+			final int i2lo = rnd.nextInt(30);
+			final int i2hi = i2lo + rnd.nextInt(4);
+			assertIntervalShl(i1lo, i1hi, i2lo, i2hi);
+		}
+	}
+
+	/**
+	 * A helper to judge the gap between soundness and precision for bit-or. This actually computes
+	 * all "or" combinations (may take a long time depending on the range). It will fail when
+	 * unsound. It will also print out the actual "precise" and "approximated" solution, for whoever
+	 * cares to read it.
+	 * 
+	 * @param i1lo
+	 * @param i1hi
+	 * @param i2lo
+	 * @param i2hi
+	 */
+	public void assertIntervalShl(final int i1lo, final int i1hi, final int i2lo, final int i2hi) {
+		Interval interval1 = new Interval(i1lo, i1hi);
+		Interval interval2 = new Interval(i2lo, i2hi);
+		Interval shlInterval = Interval.shiftLeft(interval1, interval2);
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		for (int i1 = i1lo; i1 <= i1hi; i1++) {
+			for (int i2 = i2lo; i2 <= i2hi; i2++) {
+				final int concreteOr = i1 << i2;
+				if (!shlInterval.covers(concreteOr)) {
+					Assert.fail("failed " + interval1 + " << " + interval2 + " apr " + shlInterval + " exp " + concreteOr);
+				}
+				min = Math.min(min, concreteOr);
+				max = Math.max(max, concreteOr);
+			}
+		}
+		Interval precise = new Interval(min, max);
+		System.out.println("testing " + interval1 + " << " + interval2 + " is " + precise + " apr " + shlInterval);
 	}
 
 }
